e5e1e57dc085adad0084cac65b9326746f7dcfea
YUNUSULRTA123 <afsananamazova01@gmail.com>
2024-12-22 11:58:54 +0400

    Update main (2).py

--- main (2).py
+++ main (2).py
@@ -1,18 +1,157 @@
 import telebot  
-      
-    # Инициализация бота с использованием его токена  
-    bot = telebot.TeleBot("8119500631:AAHDitnnXOQOw--jbpbgLmS4bOx_SK7LN9E")  
-      
-    # Обработчик команды '/start' и '/hello'  
-    @bot.message_handler(commands=['start', 'hello'])  
-    def send_welcome(message):  
-        bot.reply_to(message, f'Привет! Я бот {bot.get_me().first_name}!')  
-      
-    # Обработчик команды '/heh'  
-    @bot.message_handler(commands=['heh'])  
-    def send_heh(message):  
-        count_heh = int(message.text.split()[1]) if len(message.text.split()) > 1 else 5  
-        bot.reply_to(message, "he" * count_heh)  
-      
-    # Запуск бота  
-    bot.polling()
+
+import time, threading, schedule
+from telebot import TeleBot
+
+API_TOKEN = '<api_token>'
+bot = TeleBot(API_TOKEN)
+
+from telebot.types import (
+    ReplyKeyboardMarkup, 
+    KeyboardButton, 
+    WebAppInfo,
+    InlineKeyboardMarkup,
+    InlineKeyboardButton
+)
+
+BOT_TOKEN = "" 
+WEB_URL = "https://pytelegrambotminiapp.vercel.app" 
+
+bot = TeleBot(BOT_TOKEN)
+
+@bot.message_handler(commands=["start"])
+def start(message):
+    reply_keyboard_markup = ReplyKeyboardMarkup(resize_keyboard=True)
+    reply_keyboard_markup.row(KeyboardButton("Start MiniApp", web_app=WebAppInfo(WEB_URL)))
+
+    inline_keyboard_markup = InlineKeyboardMarkup()
+    inline_keyboard_markup.row(InlineKeyboardButton('Start MiniApp', web_app=WebAppInfo(WEB_URL)))
+
+    bot.reply_to(message, "Click the bottom inline button to start MiniApp", reply_markup=inline_keyboard_markup)
+    bot.reply_to(message, "Click keyboard button to start MiniApp", reply_markup=reply_keyboard_markup)
+
+@bot.message_handler(content_types=['web_app_data'])
+def web_app(message):
+    bot.reply_to(message, f'Your message is "{message.web_app_data.data}"')
+
+bot.infinity_polling()
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    bot.reply_to(message, "Hi! Use /set <seconds> to set a timer")
+
+
+def beep(chat_id) -> None:
+    """Send the beep message."""
+    bot.send_message(chat_id, text='Beep!')
+
+
+@bot.message_handler(commands=['set'])
+def set_timer(message):
+    args = message.text.split()
+    if len(args) > 1 and args[1].isdigit():
+        sec = int(args[1])
+        schedule.every(sec).seconds.do(beep, message.chat.id).tag(message.chat.id)
+    else:
+        bot.reply_to(message, 'Usage: /set <seconds>')
+
+
+@bot.message_handler(commands=['unset'])
+def unset_timer(message):
+    schedule.clear(message.chat.id)
+
+
+if __name__ == '__main__':
+    threading.Thread(target=bot.infinity_polling, name='bot_infinity_polling', daemon=True).start()
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+# Инициализация бота с использованием его токена  
+bot = telebot.TeleBot("8119500631:AAHDitnnXOQOw--jbpbgLmS4bOx_SK7LN9E")  
+
+# Обработчик команды '/start' и '/hello'  
+@bot.message_handler(commands=['start', 'hello'])  
+def send_welcome(message):  
+bot.reply_to(message, f'Привет! Я бот {bot.get_me().first_name}!')  
+
+# Обработчик команды '/heh'  
+@bot.message_handler(commands=['heh'])  
+def send_heh(message):  
+count_heh = int(message.text.split()[1]) if len(message.text.split()) > 1 else 5  
+bot.reply_to(message, "he" * count_heh)  
+text_messages = {
+    'welcome':
+        u'Please welcome {name}!\n\n'
+        u'This chat is intended for questions about and discussion of the pyTelegramBotAPI.\n'
+        u'To enable group members to answer your questions fast and accurately, please make sure to study the '
+        u'project\'s documentation (https://github.com/eternnoir/pyTelegramBotAPI/blob/master/README.md) and the '
+        u'examples (https://github.com/eternnoir/pyTelegramBotAPI/tree/master/examples) first.\n\n'
+        u'I hope you enjoy your stay here!',
+
+    'info':
+        u'My name is TeleBot,\n'
+        u'I am a bot that assists these wonderful bot-creating people of this bot library group chat.\n'
+        u'Also, I am still under development. Please improve my functionality by making a pull request! '
+        u'Suggestions are also welcome, just drop them in this group chat!',
+
+    'wrong_chat':
+        u'Hi there!\nThanks for trying me out. However, this bot can only be used in the pyTelegramAPI group chat.\n'
+        u'Join us!\n\n'
+        u'https://telegram.me/joinchat/067e22c60035523fda8f6025ee87e30b'
+}
+
+if "TELEBOT_BOT_TOKEN" not in os.environ or "GROUP_CHAT_ID" not in os.environ:
+    raise AssertionError("Please configure TELEBOT_BOT_TOKEN and GROUP_CHAT_ID as environment variables")
+
+bot = telebot.AsyncTeleBot(os.environ["TELEBOT_BOT_TOKEN"])
+GROUP_CHAT_ID = int(os.environ["GROUP_CHAT_ID"])
+
+
+def is_api_group(chat_id):
+    return chat_id == GROUP_CHAT_ID
+
+
+@bot.message_handler(func=lambda m: True, content_types=['new_chat_participant'])
+def on_user_joins(message):
+    if not is_api_group(message.chat.id):
+        return
+
+    name = message.new_chat_participant.first_name
+    if hasattr(message.new_chat_participant, 'last_name') and message.new_chat_participant.last_name is not None:
+        name += u" {}".format(message.new_chat_participant.last_name)
+
+    if hasattr(message.new_chat_participant, 'username') and message.new_chat_participant.username is not None:
+        name += u" (@{})".format(message.new_chat_participant.username)
+
+    bot.reply_to(message, text_messages['welcome'].format(name=name))
+
+
+@bot.message_handler(commands=['info', 'help'])
+def on_info(message):
+    if not is_api_group(message.chat.id):
+        bot.reply_to(message, text_messages['wrong_chat'])
+        return
+
+    bot.reply_to(message, text_messages['info'])
+
+
+@bot.message_handler(commands=["ping"])
+def on_ping(message):
+    bot.reply_to(message, "Still alive and kicking!")
+
+
+@bot.message_handler(commands=['start'])
+def on_start(message):
+    if not is_api_group(message.chat.id):
+        bot.reply_to(message, text_messages['wrong_chat'])
+        return
+
+
+def listener(messages):
+    for m in messages:
+        print(str(m))
+
+
+bot.set_update_listener(listener)
+# Запуск бота  
+bot.polling()
